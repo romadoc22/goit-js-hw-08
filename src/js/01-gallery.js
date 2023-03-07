6eb25d14988b139aa5b94de76d60d3851b27b50c
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-03-07 21:44:39 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li><a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a></li>`;
+  })
+  .join(' ');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 
 console.log(galleryItems);
