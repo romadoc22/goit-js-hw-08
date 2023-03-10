1335456aa5116756a5a34f9a393eeee148948ff0
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-03-10 22:00:43 +0200

    2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOCURTIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+if (localStorage.getItem(VIDEOCURTIME)) {
+  const playedTime = Number(localStorage.getItem(VIDEOCURTIME));
+  player.setCurrentTime(playedTime);
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => localStorage.setItem(VIDEOCURTIME, data.seconds), 1000)
+);
